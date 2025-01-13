2247938ca7dd590e195372532c1952071859e4b0
Tenzin Kunga <officialtenzin.kunga@gmail.com>
2025-01-12 19:10:15 -0600

    create emojipedia using mapping_components in React

---
+++ src/emojipedia.js
@@ -0,0 +1,43 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:"A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object. This emoji is often used in romantic messages to express love, gratitude, and admiration. It can also be used to convey enthusiasm for a cause or event. The heart-shaped eyes symbolize overwhelming feelings of happiness and a strong connection."
+  },
+  {
+    id: 5,
+    emoji: "🥰",
+    name: "Smiling Face With smiling eyes and three hearts ",
+    meaning:"This adorable 🥰 emoji represents a smiling face surrounded by hearts, embodying infatuation and romance. It's perfect for expressing affection, love and romantic affairs of the heart while floating on cloud nine. Deep connection and loving feelings can be conveyed to the person you are talking to through this emoji. It can also be used to express appreciation for special moments or heartfelt gestures. All in all, this emoji is a wonderful way to express your love and affection in digital communication."
+  },
+  {
+    id: 6,
+    emoji: "😘",
+    name: "Face Throwing a Kiss",
+    meaning:"The kiss emoji 😘 is an expression of affection. It conveys friendly or romantic sympathy and is often used to express tenderness or as a flirtatious signal. The kissing emoji can also be used to express gratitude or to soften a sarcastic response. It is also an appropriate response to rude messages to lighten the situation. This emoji is a charming way to share feelings and emotions, and convey closeness and connection even when you are physically separated. Use it to give your loved ones a little smile and show them you care."
+  }
+];
+
+export default emojipedia;
